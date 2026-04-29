6a5bb6cd1a554ac9e36abc785c84a0c711b595bb
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-29 18:56:18 +0000

    quest_2

---
+++ quest_2.py
@@ -0,0 +1,8 @@
+# 2) Verificação de Número Par/Ímpar: Crie um programa que pede ao usuário um número e verifica se ele é par ou ímpar.
+
+numero = int(input("Digite o número: "))
+if numero % 2 == 0:
+    print("O número é par.")
+
+else:
+    print("O número é ímpar.")
